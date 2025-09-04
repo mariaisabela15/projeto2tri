10e23ff17142c98431677ddd92f728b0bc5da8e4
mariaisabela15 <145874020+mariaisabela15@users.noreply.github.com>
2025-09-04 11:52:14 -0300

    .

--- script.js
+++ script.js
@@ -4,3 +4,53 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    {
+        enunciado: "Você imagina sua vida sem a IA?",
+        alternativas: ["SIM","NÃO"],
+    },
+
+{
+   enunciado: "Você já utilizou a IA como ferramenta de estudo ou apenas para resolver as tarefas?",
+   alternativas: ["SIM","NÃO"],
+},
+
+{
+    enunciado: "A IA te assusta em algum aspecto? Como na criação de imagens/vídeos?",
+   alternativas: ["SIM","NÃO"],
+},
+
+{
+    enunciado: "Você acha que a IA pode se tornar uma ameaça para a humanidade?",
+   alternativas: ["SIM","NÃO"],
+},
+
+{
+    enunciado: "Você acredita que a  IA está deixando as pessoas mais dependentes da tecnologia?",
+   alternativas: ["SIM","NÃO"],
+},
+
+{
+    enunciado: "Você já preferiu conversar com uma IA do que com uma pessoa/amigo?",
+   alternativas: ["SIM","NÃO"],
+},
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaatual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
