309d9e3bc97740450052cb968aa8b264a883ff88
mariaisabela15 <145874020+mariaisabela15@users.noreply.github.com>
2025-10-09 11:23:01 -0300

    .

--- script.js
+++ script.js
@@ -46,15 +46,18 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
+
+
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
+function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal = afirmacoes + " ";
     atual++;
@@ -63,6 +66,7 @@ function respostaSelecionada(opcaoSelecionada){
 
 function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "",
 }
 
