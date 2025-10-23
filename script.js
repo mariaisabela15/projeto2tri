5b582d1ad528915140c430b7f2b99c4b08469a56
mariaisabela15 <145874020+mariaisabela15@users.noreply.github.com>
2025-10-23 10:49:11 -0300

    .

--- script.js
+++ script.js
@@ -7,33 +7,28 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Você imagina sua vida sem a IA?",
-        alternativas: ["SIM","NÃO"],
+        alternativas: ["SIM", "NÃO"],
     },
-
-{
-   enunciado: "Você já utilizou a IA como ferramenta de estudo ou apenas para resolver as tarefas?",
-   alternativas: ["SIM","NÃO"],
-},
-
-{
-    enunciado: "A IA te assusta em algum aspecto? Como na criação de imagens/vídeos?",
-   alternativas: ["SIM","NÃO"],
-},
-
-{
-    enunciado: "Você acha que a IA pode se tornar uma ameaça para a humanidade?",
-   alternativas: ["SIM","NÃO"],
-},
-
-{
-    enunciado: "Você acredita que a  IA está deixando as pessoas mais dependentes da tecnologia?",
-   alternativas: ["SIM","NÃO"],
-},
-
-{
-    enunciado: "Você já preferiu conversar com uma IA do que com uma pessoa/amigo?",
-   alternativas: ["SIM","NÃO"],
-},
+    {
+        enunciado: "Você já utilizou a IA como ferramenta de estudo ou apenas para resolver as tarefas?",
+        alternativas: ["SIM", "NÃO"],
+    },
+    {
+        enunciado: "A IA te assusta em algum aspecto? Como na criação de imagens/vídeos?",
+        alternativas: ["SIM", "NÃO"],
+    },
+    {
+        enunciado: "Você acha que a IA pode se tornar uma ameaça para a humanidade?",
+        alternativas: ["SIM", "NÃO"],
+    },
+    {
+        enunciado: "Você acredita que a IA está deixando as pessoas mais dependentes da tecnologia?",
+        alternativas: ["SIM", "NÃO"],
+    },
+    {
+        enunciado: "Você já preferiu conversar com uma IA do que com uma pessoa/amigo?",
+        alternativas: ["SIM", "NÃO"],
+    },
 ];
 
 let atual = 0;
@@ -44,35 +39,33 @@ function mostraPergunta() {
     if (atual >= perguntas.length) {
         mostraResultado();
         return;
-}
+    }
 
-perguntaAtual = perguntas[atual];
-caixaPerguntas.textContent = perguntaAtual.enunciado;
-caixaAlternativas.textContent = "";
-mostraAlternativas();
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
-
 
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.textContent = alternativa;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal = afirmacoes + " ";
+    historiaFinal += opcaoSelecionada + " ";
     atual++;
     mostraPergunta();
 }
-;
-function mostraResultado(){
+
+function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "",
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
